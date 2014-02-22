f0900032e75a7db70a996ae8a1f1ac66a77d3e85
Teemu Puukko <teemuki@gmail.com>
2014-02-22 14:56:25 +0200

    Added a balls

--- app/js/keinupallo/main.js
+++ app/js/keinupallo/main.js
@@ -1,6 +1,17 @@
 (function() {
+	$(function() {
+		var stage = new createjs.Stage("KeinuPalloCanvas");
 
-	console.log('main!');
+		var circle = new createjs.Shape();
+		
+		circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+		circle.x = 100;
+		circle.y = 100;
 
+		stage.addChild(circle);
 
+		stage.update();
+
+		console.log("skdjfls");
+	});
 }());
